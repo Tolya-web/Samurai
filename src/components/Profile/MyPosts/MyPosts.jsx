7ff7de98461fc2c8136d459cf6d7f11f422604b3
Tolya <Alinka87@inbox.ru>
2021-07-27 19:00:31 +0300

    Kabzda

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
-import { addPostActionCreator, updateNewPostTextActionCreator } from '../../../redux/profile-reducer';
 import s from './MyPosts.module.css';
 import Post from './Post/Post';
-
-
 
 
 const MyPosts = (props) => {
@@ -14,17 +11,13 @@ let postsElements =
 
 let newPostElement = React.createRef();
 
-let addPost = () => {
-  props.dispatch(addPostActionCreator());
- 
+let onAddPost = () => {
+ props.addPost();
 }
 
 let onPostChange = () =>{
   let text = newPostElement.current.value;
- // let action = { type: 'UPDATE-NEW-POST-TEXT', newText: text};
- let action = updateNewPostTextActionCreator(text);
-  props.dispatch(action);
-
+  props.updateNewPostText(text);
 }
 
   return (
@@ -36,7 +29,7 @@ let onPostChange = () =>{
               value={props.newPostText} />
     </div>
     <div>
-    <button onClick = { addPost }>Add post</button>
+    <button onClick = { onAddPost }>Add post</button>
     </div>
   </div>
   <div className={s.posts}>
